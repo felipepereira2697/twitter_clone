69d5d9fc9b525e28086aec83d7028274d3be2b0f
Felipe Pereira <felipepereira2697@gmail.com>
2017-02-27 23:08:45 -0300

    follow/unfollow pt 2

--- get_pessoas.php
+++ get_pessoas.php
@@ -16,7 +16,11 @@ $link = $objDb->conecta_mysql();
 
 //a clausula AND id<> $id_usuario é para evitar que traga vc como resultado de alguém que pode seguir, 
 //não tem lógica vc seguir vc mesmo
-$sql = " SELECT * FROM usuarios WHERE usuario LIKE '%$nome_pessoa%' AND id <> $id_usuario";
+$sql = 	" SELECT u.*, us.* ";
+$sql .= " FROM usuarios AS u ";
+$sql .= " LEFT JOIN usuarios_seguidores AS us ";
+$sql .= " ON (us.id_usuario = $id_usuario AND u.id = us.seguindo_id_usuario) ";
+$sql .= " WHERE u.usuario LIKE '%$nome_pessoa%' AND u.id <> $id_usuario ";
 
 //resultado id contém a referencia p/ o conteúdo externo ao php
 $resultado_id = mysqli_query($link,$sql);
@@ -31,8 +35,22 @@ if($resultado_id){
 				//para pegar o id de quem queremos seguir, vamos utilizar umm atributo customizável
 				//para manipular com JS
 				//para que cada um dos botões tenha esse atributo data-id_usuario com o id_usuario do banco de dados
-				echo '<button type="button" id="btn_seguir_'.$registro['id'].'" class="btn btn-primary btn_seguir" data-id_usuario="'.$registro['id'].'">Seguir</button>';
-				echo '<button type="button" id="btn_deixar_seguir_'.$registro['id'].'" style="display:none;" class="btn btn-danger btn_deixar_seguir" data-id_usuario="'.$registro['id'].'">Deixar de seguir</button>';
+
+				//criar uma variavel para verificar se o registro possui uma variavel id_usuario_seguidor
+				$esta_seguindo_usuario_sn = isset($registro['id_usuario_seguidor']) && !empty($registro['id_usuario_seguidor']) ? 'S' : 'N'; 
+
+				$btn_seguir_display = 'block';
+				$btn_deixar_seguir_display = 'block';
+				//com base nesse teste saberemos quem devemos ocultar
+				if($esta_seguindo_usuario_sn == 'S'){
+					$btn_seguir_display = 'none';
+
+				}else{
+					$btn_deixar_seguir_display = 'none';
+				}
+				//concatenmaos o registro['id'] no id do botão para que cada botão tenha uma identidade única
+				echo '<button type="button" id="btn_seguir_'.$registro['id'].'" style="display:'.$btn_seguir_display.'"  class="btn btn-primary btn_seguir" data-id_usuario="'.$registro['id'].'">Seguir</button>';
+				echo '<button type="button" id="btn_deixar_seguir_'.$registro['id'].'" style="display:'.$btn_deixar_seguir_display.'" class="btn btn-danger btn_deixar_seguir" data-id_usuario="'.$registro['id'].'">Deixar de seguir</button>';
 			echo '</p>';
 			//quando usamos o pull right o elemento perde a noção do espaçamento, para isso usaremos o clearfix
 			echo '<div class="clearfix"></div>';
